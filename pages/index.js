93e169e908126d5151e4488b4ce64c48b1c20b34
Calvin Li <callvinlinz@gmail.com>
2022-05-18 20:54:17 +1200

    prop test

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-var data = require("../crosshairs.json");
+const data = require("../crosshairs.json");
 
 for(var i =0; i < data.length; i++){
   var obj = data[i];
     console.log(`name:  ${obj.player_info.name}, Team: ${obj.player_info.team}`);
   }
-  
+
+export const getStaticProps = async() =>{
+  return {
+    props: {Crosshairs:data}
+  }
+}
 
 
 
-export default function Home() {
+export default function Home({Crosshairs}) {
   return (
     <div className = "page">
       <Head>
@@ -24,6 +29,13 @@ export default function Home() {
         <div className = {styles.header}>
           <h1>Crosshair Website</h1>
           <p>Welcome to my crosshair website for Valorant</p>
+          {Crosshairs.map(Crosshair => (
+            <div key={Crosshair.player_info.name}>
+              <a>
+                <h3>{`Name: ${Crosshair.player_info.name} | Team: ${Crosshair.player_info.team} `}</h3>
+              </a>
+            </div>
+          ))}
         </div>
 
         
