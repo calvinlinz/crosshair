a72b786ece68a4ec3e1ff3fa45bc49ea92c5737c
Calvin Li <callvinlinz@gmail.com>
2022-09-03 18:29:46 +1200

    added team function

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import React from 'react';
 import CrosshairCanvas from "../Crosshair/CrosshairCanvas";
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {SketchPicker} from 'react-color';
 
 export const getStaticProps = async () => {
   const data = require("../Crosshair/crosshairs.json");
@@ -16,15 +17,32 @@ export const getStaticProps = async () => {
 };
 
 
-
-
 export default function Home({ Crosshairs }) {
   const [query, setQuery] = useState("");
   const [background, setBackground] = useState("");
+  const [currentTeam, setTeam] = useState("");  
 
+
+  const teams = ["All"];
+  let count = 1;
+  Crosshairs.forEach((crosshair) =>{
+    if(!teams.includes(crosshair.player_info.team)){
+      teams[count] = crosshair.player_info.team;
+      count++;
+    }
+  })
+
+  function handleTeam(event){
+    setTeam({value:event.target.value});
+    console.log({value:event.target.value})
+  }
+
+  //WIP
   function handleChange(event) {
     setBackground({value: event.target.value});
   }
+
+
   return (
     <div className="page">
       <Head>
@@ -45,29 +63,45 @@ export default function Home({ Crosshairs }) {
               <option value = "custom">Custom</option>
               <option value = "range">Range</option>
              </select>
+
+          
+          <div className = {styles.teams} >
+            <label>Team</label>
+            <select name = "teamSelector" value = {currentTeam.value} placeholder = {currentTeam} onChange={handleTeam}>
+              {teams.map((team)=>{
+                  return <option value ={team}> {team}</option>
+                })
+              }
+              </select>
+          </div>
+            
+          
         </div>
         <div className={styles.gridContainer}>
           {Crosshairs.filter((Crosshair) => {
-            if (query === "") {
+            if(currentTeam.value === "All"){
+              if (query === "" ) {
+                return Crosshair;
+              } else if (Crosshair.player_info.name.toLowerCase().startsWith(query.toLowerCase())) {
+                return Crosshair;
+              }
+          }else{
+            if (query === "" && currentTeam.value === Crosshair.player_info.team) {
               return Crosshair;
-            } else if (
-              Crosshair.player_info.name
-                .toLowerCase()
-                .startsWith(query.toLowerCase())
-            ) {
+            } else if (Crosshair.player_info.name.toLowerCase().startsWith(query.toLowerCase()) && currentTeam.value === Crosshair.player_info.team) {
               return Crosshair;
             }
+          }
           }).map((Crosshair) => (
             <div key={Crosshair.player_info.name}>
               <div className={styles.gridBox}>
                 <div className={styles.gridBoxTop}>
                   <h3>{`${Crosshair.player_info.name} | ${Crosshair.player_info.team} `}</h3>
                   <button  onClick={() => {
-                                        navigator.clipboard.writeText(jsonToVal(Crosshair))
-                                        toast(`Copied ${Crosshair.player_info.name}'s crosshair.`,{
-                                          theme: "light"
-                                        })
-                                    }}>Copy</button>
+                    navigator.clipboard.writeText(jsonToVal(Crosshair))
+                    toast(`Copied ${Crosshair.player_info.name}'s crosshair.`,{
+                    })
+                  }}>Copy</button>
                 </div>
                 <div className={styles.square}>
                   <CrosshairCanvas player = {Crosshair}/>
